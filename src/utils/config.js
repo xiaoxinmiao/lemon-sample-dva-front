978e49c7fd31d50051c90da0c52e38e5952ac743
xiao.xinmiao <xiaoxm_001@163.com>
2018-05-16 12:17:55 +0800

    switch backend api

--- src/utils/config.js
+++ src/utils/config.js
@@ -3,16 +3,16 @@ export const backendAddr = (() => {
     const env = process.env.STAGE_ENV
     switch (env) {
       case 'staging': return {
-        sample:'https://staging.p2shop.com.cn/fruit/v1',
+        sample:'https://staging.p2shop.com.cn/sample',
       };
       case 'qa': return {
-        sample:'https://qa.p2shop.com.cn/fruit/v1',
+        sample:'https://qa.p2shop.com.cn/sample',
       };
       case 'production': return {
-        sample:'https://gateway.p2shop.com.cn/fruit/v1',
+        sample:'https://gateway.p2shop.com.cn/sample',
       };
       default: return {
-        sample:'http://localhost:8080/v1',
+        sample:'http://localhost:8080',
       };
     }
   })();
